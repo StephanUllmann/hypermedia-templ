f710a641ef8f207b6e9b06a2381734799715da52
Stephan Ullmann <stephan.ullmann@posteo.de>
2023-12-27 12:46:47 +0100

    deplomnent env

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,7 +10,7 @@ import (
 	"github.com/StephanUllmann/hypermedia-templ/routes"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -18,10 +18,10 @@ import (
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	db.Init()
 	
@@ -41,10 +41,9 @@ func main() {
 	r.Mount("/contacts", routes.ContactsRouter())
 
 	port := ":" + os.Getenv("PORT")
-	fmt.Printf("test: %v\n", os.Getenv("test"))
  
 	log.Printf("Server started on port %v\n", port)
-	err = http.ListenAndServe(port, r)
+	err := http.ListenAndServe(port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
